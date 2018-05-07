812bbfd8d7db3089b9068b09e0af903e43b3b38d
Mathew Tinu Thomas <mathewtinuthomas@gmail.com>
2018-05-07 16:01:09 +0530

    Initial commit

---
+++ DrumOn/DrumOn/DrumOnForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace DrumOn
+{
+    partial class DrumOnForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DrumOnForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(683, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold);
+            this.aboutToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // DrumOnForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::DrumOn.Properties.Resources.BackgroundImage;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(683, 516);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "DrumOnForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DrumOn";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.DrumOnForm_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
---
+++ DrumOn/DrumOn/DrumOnForm.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace DrumOn
+{
+    public partial class DrumOnForm : Form
+    {
+        MediaPlayer qBassPlayer;
+        MediaPlayer wBassPlayer;
+        MediaPlayer eSnarePlayer;
+        MediaPlayer rSnarePlayer;
+        MediaPlayer pHihatPlayer;
+        MediaPlayer xHihatPlayer;
+        MediaPlayer iOHihatPlayer;
+        MediaPlayer cOHihatPlayer;
+        MediaPlayer tTom1Player;
+        MediaPlayer yTom1Player;
+        MediaPlayer gTom2Player;
+        MediaPlayer hTom2Player;
+        MediaPlayer bFloortomPlayer;
+        MediaPlayer nFloortomPlayer;
+        MediaPlayer oCrashPlayer;
+        MediaPlayer uCrashPlayer;
+        MediaPlayer lRidePlayer;
+        MediaPlayer mRidePlayer;
+        MediaPlayer kRidecupPlayer;
+        MediaPlayer jRidecupPlayer;
+        MediaPlayer dotChinaPlayer;
+        MediaPlayer zChinaPlayer;
+        MediaPlayer commaSplashPlayer;
+        MediaPlayer aSplashPlayer;
+
+        string path = string.Empty;
+        string filename = string.Empty;
+        string extension = string.Empty;
+        public DrumOnForm()
+        {
+            InitializeComponent();
+            qBassPlayer = new MediaPlayer();
+            wBassPlayer = new MediaPlayer();
+            eSnarePlayer = new MediaPlayer();
+            rSnarePlayer = new MediaPlayer();
+            pHihatPlayer = new MediaPlayer();
+            xHihatPlayer = new MediaPlayer();
+            iOHihatPlayer = new MediaPlayer();
+            cOHihatPlayer = new MediaPlayer();
+            tTom1Player = new MediaPlayer();
+            yTom1Player = new MediaPlayer();
+            gTom2Player = new MediaPlayer();
+            hTom2Player = new MediaPlayer();
+            bFloortomPlayer = new MediaPlayer();
+            nFloortomPlayer = new MediaPlayer();
+            oCrashPlayer = new MediaPlayer();
+            uCrashPlayer = new MediaPlayer();
+            lRidePlayer = new MediaPlayer();
+            mRidePlayer = new MediaPlayer();
+            kRidecupPlayer = new MediaPlayer();
+            jRidecupPlayer = new MediaPlayer();
+            dotChinaPlayer = new MediaPlayer();
+            zChinaPlayer = new MediaPlayer();
+            commaSplashPlayer = new MediaPlayer();
+            aSplashPlayer = new MediaPlayer();
+        }
+
+        private void DrumOnForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Escape:
+                    this.Close();
+                    break;
+
+                case Keys.Q:
+                    SetUriPathVariables("DrumSounds/", "Bass", ".wav");
+                    Play(qBassPlayer);
+                    break;
+                case Keys.W:
+                    SetUriPathVariables("DrumSounds/", "Bass", ".wav");
+                    Play(wBassPlayer);
+                    break;
+
+                case Keys.E:
+                    SetUriPathVariables("DrumSounds/", "Snare", ".wav");
+                    Play(eSnarePlayer);
+                    break;
+
+                case Keys.R:
+                    SetUriPathVariables("DrumSounds/", "Snare", ".wav");
+                    Play(rSnarePlayer);
+                    break;
+
+                case Keys.P:
+                    Stop(iOHihatPlayer);
+                    Stop(cOHihatPlayer);
+                    SetUriPathVariables("DrumSounds/", "HiHat", ".wav");
+                    Play(pHihatPlayer);
+                    break;
+                case Keys.X:
+                    Stop(iOHihatPlayer);
+                    Stop(cOHihatPlayer);
+                    SetUriPathVariables("DrumSounds/", "HiHat", ".wav");
+                    Play(xHihatPlayer);
+                    break;
+
+                case Keys.I:
+                    SetUriPathVariables("DrumSounds/", "OHiHat", ".wav");
+                    Play(iOHihatPlayer);
+                    break;
+                case Keys.C:
+                    SetUriPathVariables("DrumSounds/", "OHiHat", ".wav");
+                    Play(cOHihatPlayer);
+                    break;
+
+                case Keys.T:
+                    SetUriPathVariables("DrumSounds/", "Tom1", ".wav");
+                    Play(tTom1Player);
+                    break;
+                case Keys.Y:
+                    SetUriPathVariables("DrumSounds/", "Tom1", ".wav");
+                    Play(yTom1Player);
+                    break;
+
+                case Keys.G:
+                    SetUriPathVariables("DrumSounds/", "Tom2", ".wav");
+                    Play(gTom2Player);
+                    break;
+                case Keys.H:
+                    SetUriPathVariables("DrumSounds/", "Tom2", ".wav");
+                    Play(hTom2Player);
+                    break;
+
+                case Keys.B:
+                    SetUriPathVariables("DrumSounds/", "FloorTom", ".wav");
+                    Play(bFloortomPlayer);
+                    break;
+                case Keys.N:
+                    SetUriPathVariables("DrumSounds/", "FloorTom", ".wav");
+                    Play(nFloortomPlayer);
+                    break;
+
+                case Keys.O:
+                    SetUriPathVariables("DrumSounds/", "Crash", ".wav");
+                    Play(oCrashPlayer);
+                    break;
+                case Keys.U:
+                    SetUriPathVariables("DrumSounds/", "Crash", ".wav");
+                    Play(uCrashPlayer);
+                    break;
+
+                case Keys.L:
+                    SetUriPathVariables("DrumSounds/", "Ride", ".wav");
+                    Play(lRidePlayer);
+                    break;
+                case Keys.M:
+                    SetUriPathVariables("DrumSounds/", "Ride", ".wav");
+                    Play(mRidePlayer);
+                    break;
+
+                case Keys.K:
+                    SetUriPathVariables("DrumSounds/", "RideCup", ".wav");
+                    Play(kRidecupPlayer);
+                    break;
+                case Keys.J:
+                    SetUriPathVariables("DrumSounds/", "RideCup", ".wav");
+                    Play(jRidecupPlayer);
+                    break;
+
+                case Keys.OemPeriod:
+                    SetUriPathVariables("DrumSounds/", "China", ".wav");
+                    Play(dotChinaPlayer);
+                    break;
+                case Keys.Z:
+                    SetUriPathVariables("DrumSounds/", "China", ".wav");
+                    Play(zChinaPlayer);
+                    break;
+
+                case Keys.Oemcomma:
+                    SetUriPathVariables("DrumSounds/", "Splash", ".wav");
+                    Play(commaSplashPlayer);
+                    break;
+                case Keys.A:
+                    SetUriPathVariables("DrumSounds/", "Splash", ".wav");
+                    Play(aSplashPlayer);
+                    break;
+            }
+        }
+
+
+        void Play(MediaPlayer player)
+        {
+            Uri uriSource = new Uri(CreatePath(path, filename, extension), UriKind.Relative);
+            player.Open(uriSource);
+            player.Play();
+        }
+
+        void Stop(MediaPlayer player)
+        {
+            player.Stop();
+        }
+
+        string CreatePath(string path, string filename, string extension)
+        {
+            return path + filename + extension;
+        }
+
+        void SetUriPathVariables(string pPath, string pFilename, string pExtension)
+        {
+            path = pPath;
+            filename = pFilename;
+            extension = pExtension;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Version: 1.0\nCrafted By: Mathew Tinu Thomas\nDrop your comments to mathewtinuthomas@gmail.com.\nHappy drumming! :)","DrumOn");
+        }
+    }
+}
